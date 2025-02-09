ccebadaa54426bda626df0e273711defbf5b7cce
Jacques Mayol <jacques@ocean.sea>
2025-02-09 22:20:08 +0100

    Print game board

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include "Logger.h"
 
+#include <iomanip>
+
 int main()
 {
-    logI << __func__ << ": main is first thread function";
+    const int height = 31;
+    const int width = 61;
+
+    std::cout << "  ";
+
+    for (int x = 0; x < width; x++)
+    {
+        auto color = x % 2 ? vmo::magenta : vmo::cyan;
+
+        std::cout << color << std::setfill('0') << std::setw(2) << x << vmo::end;
+    }
+
+    std::cout << '\n';
+
+    for (int y = 0; y < height; y++)
+    {
+        auto color = y % 2 ? vmo::magenta : vmo::cyan;
+
+        std::cout << color << std::setfill('0') << std::setw(2) << y << vmo::end;
+
+        for (int x = 0; x < width; x++)
+        {
+            std::cout << ' ' << '+';
+        }
+
+        std::cout << '\n';
+    }
 }
