087a53d5d7b70d44b99f0f5135e737c266228158
Jacques Mayol <jacques@ocean.sea>
2025-02-22 13:51:12 +0100

    Remove bastet ui

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include "Curses.hpp"
 #include "Game.hpp"
-#include "Ui.hpp"
-
-#include <iostream>
 
 int main()
 {
-    // Bastet::Ui ui;
-    // ui.Play();
-
     try
     {
         Bastet::Curses curses;
@@ -18,9 +12,6 @@ int main()
     catch (Bastet::GameOver & go)
     {
     }
-
-    char c;
-    std::cin >> c;
 
     return 0;
 }
