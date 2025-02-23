af7777d8c2ea10e37634140200ea7f40c100397f
Jacques Mayol <jacques@ocean.sea>
2025-02-23 10:36:37 +0100

    Add water area

--- src/Silver/Water.cpp
+++ src/Silver/Water.cpp
@@ -3,8 +3,11 @@
 namespace Silver
 {
 
-Water::Water()
+Water::Water(int width, int height)
+    : _width{width}
+    , _height{height}
 {
+    _water.resize(_height, std::vector<int>(_width, 0));
 }
 
 Water::~Water()
--- src/Silver/Water.hpp
+++ src/Silver/Water.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
+#include <vector>
+
 namespace Silver
 {
 
 class Water
 {
 public:
-    Water();
+    Water(int width, int height);
     ~Water();
 
 private:
     int _width;
     int _height;
+
+    std::vector<std::vector<int>> _water;
 };
 
 } // namespace Silver
