756f457d5b69a4ec7c484dd8e4ab46422caee59e
Jacques Mayol <jacques@ocean.sea>
2025-02-24 18:50:31 +0100

    Init colors

--- src/Bastet/Engine.cpp
+++ src/Bastet/Engine.cpp
@@ -41,14 +41,14 @@ Engine::Engine()
     cbreak();
 
     start_color();
-    /* 1 - 16 is for blocks */
-    init_pair(1, COLOR_BLACK, COLOR_RED);
-    init_pair(2, COLOR_BLACK, COLOR_GREEN);
-    init_pair(3, COLOR_BLACK, COLOR_YELLOW);
-    init_pair(4, COLOR_BLACK, COLOR_BLUE);
-    init_pair(5, COLOR_BLACK, COLOR_MAGENTA);
-    init_pair(6, COLOR_BLACK, COLOR_CYAN);
-    init_pair(7, COLOR_BLACK, COLOR_WHITE);
+    /* 0 - 16 is for blocks */
+    init_pair(0, COLOR_WHITE, COLOR_BLACK);
+    init_pair(1, COLOR_WHITE, COLOR_RED);
+    init_pair(2, COLOR_WHITE, COLOR_GREEN);
+    init_pair(3, COLOR_WHITE, COLOR_YELLOW);
+    init_pair(4, COLOR_WHITE, COLOR_BLUE);
+    init_pair(5, COLOR_WHITE, COLOR_MAGENTA);
+    init_pair(6, COLOR_WHITE, COLOR_CYAN);
 
     /* 17 - ? is for other things */
     init_pair(17, COLOR_RED, COLOR_BLACK);    // points
